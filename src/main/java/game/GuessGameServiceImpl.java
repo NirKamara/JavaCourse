1cc979c0dc336bde0764b4b71c8d2db6440a08e9
NirKamara <nirkamara@gmail.com>
2016-11-30 16:31:07 +0200

    fghfg

---
+++ src/main/java/game/GuessGameServiceImpl.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ * Created by student14 on 30/11/2016.
+ */
+public class GuessGameServiceImpl implements GuessGameService {
+
+    public void play(int max) {
+
+    }
+
+    public void printBestScore() {
+
+    }
+}
